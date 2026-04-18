1acdca2cc223f860e045317f229347855908640b
Eric Florenzano <floguy@gmail.com>
2026-04-18 07:16:24 -0700

    desktop: fire native notification when server enters Error state (#148)
    Co-authored-by: Cloud Eric <clouderic@eflorenzano.com>

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -370,6 +370,7 @@ fn main() {
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_autostart::init(
             MacosLauncher::LaunchAgent,
             None,
